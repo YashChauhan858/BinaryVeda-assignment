b5dd10541e51b0bb9076d28d443dd9245a8e5969
Yash Chauhan <yashchauhan858@gmail.com>
2023-10-11 12:51:13 +0530

    Added responsive design for header

--- src/components/Global/Header/Header.tsx
+++ src/components/Global/Header/Header.tsx
@@ -3,7 +3,7 @@ import marvelWhite from "@/assets/logo-white.svg";
 
 const Header = () => {
   return (
-    <section className="p-3 flex justify-between items-center select-none">
+    <section className="p-3 sm:p-8 flex justify-between items-center select-none">
       {/* Logo section */}
       <div className="flex gap-2 items-center">
         <img src={marvelWhite} className="h-7" alt="marvel-developers-logo" />
@@ -11,10 +11,20 @@ const Header = () => {
         <h4 className="text-textColor font-semibold">Developers</h4>
       </div>
       {/* Hamburger menu */}
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 sm:hidden">
         <span className="w-6 h-1 bg-textColor rounded-lg"></span>
         <span className="w-6 h-1 bg-textColor rounded-lg"></span>
       </div>
+      {/* Nav options ( sm > ) */}
+      <nav className="hidden sm:block">
+        <ul className="flex list-none gap-5">
+          <li className="text-textColor cursor-pointer">Why marvel</li>
+          <li className="text-textColor cursor-pointer">Enterprice</li>
+          <li className="text-textColor cursor-pointer">Pricing</li>
+          <li className="text-textColor cursor-pointer">Sign in</li>
+          <li className="text-textColor cursor-pointer">Button</li>
+        </ul>
+      </nav>
     </section>
   );
 };
