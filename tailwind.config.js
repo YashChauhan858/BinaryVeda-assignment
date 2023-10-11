17e76cc11aff0acdc9d265d6f212c765eb53aa8a
Yash Chauhan <yashchauhan858@gmail.com>
2023-10-11 12:19:55 +0530

    Added color classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundColor: "#081110",
+        textColor: "#e2e8f0",
+        textColorMuted: "#8e9cb1",
+      },
+    },
   },
   plugins: [],
 };
