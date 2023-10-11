39ddd31aac7036791397802fb3f5990e9b66ace4
Yash Chauhan <yashchauhan858@gmail.com>
2023-10-11 14:48:57 +0530

    added feature card

---
+++ src/components/Home/FeatureCards/FeatureCards.tsx
@@ -0,0 +1,25 @@
+interface IFeatureCards {
+  image?: string;
+  imageAlt?: string;
+  header?: string;
+  description?: string;
+}
+
+const FeatureCards = ({
+  image = "",
+  imageAlt = "",
+  header = "",
+  description = "",
+}: IFeatureCards) => {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <img src={image} alt={imageAlt} className="object-contain w-60 sm:w-48" />
+      <h3 className="text-textColor text-lg font-medium mt-5">{header}</h3>
+      <p className="text-textColorMuted text-sm font-medium text-center mt-5">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default FeatureCards;
