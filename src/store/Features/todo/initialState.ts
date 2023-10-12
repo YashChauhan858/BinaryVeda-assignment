dbec59a1f5b4f10e0028176abf88403ec0d8bd6a
Yash Chauhan <yashchauhan858@gmail.com>
2023-10-12 12:42:07 +0530

    Added dummy data for todoList

--- src/store/Features/todo/initialState.ts
+++ src/store/Features/todo/initialState.ts
@@ -1,5 +1,25 @@
-interface IinitialState {}
+interface IinitialState {
+  todo: todoItem;
+}
 
-const initialState: IinitialState = {};
+type todoItem = { id: number; status: "DONE" | "PENDING"; task: string }[];
+
+const initialState: IinitialState = {
+  todo: [
+    { id: 1, status: "DONE", task: "bring groceries" },
+    { id: 1, status: "DONE", task: "bring groceries" },
+    { id: 1, status: "DONE", task: "bring groceries" },
+    { id: 1, status: "DONE", task: "bring groceries" },
+    { id: 1, status: "DONE", task: "bring groceries" },
+    { id: 2, status: "PENDING", task: "do assignment" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+    { id: 3, status: "PENDING", task: "do assignment 2" },
+  ],
+};
 
 export default initialState;
