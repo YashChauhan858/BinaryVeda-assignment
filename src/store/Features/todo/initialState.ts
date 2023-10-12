c67b45bfd1188f6f48a710e512038a68f1459236
Yash Chauhan <yashchauhan858@gmail.com>
2023-10-12 13:41:36 +0530

    Cleared todo initial state

--- src/store/Features/todo/initialState.ts
+++ src/store/Features/todo/initialState.ts
@@ -6,13 +6,6 @@ export type todoItem = { id: string; status: "DONE" | "PENDING"; task: string };
 
 const initialState: IinitialState = {
   todo: [
-    { id: "1", status: "DONE", task: "bring groceries" },
-    { id: "2", status: "DONE", task: "bring groceries" },
-    { id: "3", status: "DONE", task: "bring groceries" },
-    { id: "4", status: "DONE", task: "bring groceries" },
-    { id: "5", status: "DONE", task: "bring groceries" },
-    { id: "6", status: "PENDING", task: "do assignment" },
-    { id: "7", status: "PENDING", task: "do assignment 2" },
   ],
 };
 
